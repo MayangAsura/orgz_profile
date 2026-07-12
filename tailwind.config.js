f2c1dbe3e96e838d570aa7186e6a4fb891109406
MayangAsura <mayang.asura123@gmail.com>
2026-07-12 20:26:15 +0700

    updated tailwind version, postcss, autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  darkMode: 'class',
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        blueGray: {
-          200: '#e2e8f0',
-          300: '#cbd5e1',
-          400: '#94a3b8',
-          500: '#64748b',
-          600: '#475569',
-          700: '#334155',
-          800: '#1e293b',
-        },
-        lightBlue: {
-          300: '#7dd3fc',
-          400: '#38bdf8',
-          500: '#0ea5e9',
-          600: '#0284c7',
-        }
-      },
-      minHeight: {
-        'screen-75': '75vh',
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')({
-        datatables: true,
-    }),
-  ],
+  plugins: [],
 }
 
